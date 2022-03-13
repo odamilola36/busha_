7b808a3001be40aa30466cf5fdb5218b0651fe30
odamilola36 <Odamilola36@gmail.com>
2022-03-14 00:12:22 +0100

    update swagger doc spec-01

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func main() {
 
 	r := mux.NewRouter()
 	r.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
-		httpSwagger.URL("http://localhost:8080/swagger/doc.json"), //The url pointing to API definition
+		httpSwagger.URL("http://52.207.233.252:8080/swagger/doc.json"), //The url pointing to API definition
 		//httpSwagger.DeepLinking(true),
 		//httpSwagger.DocExpansion("none"),
 		//httpSwagger.DomID("#swagger-ui"),
